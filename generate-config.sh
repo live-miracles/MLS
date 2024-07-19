50c4b46591500a261cd212bebbdbdb8ab42bf5c3
AlexFreik <alexfreik5@gmail.com>
2024-07-19 19:58:29 +0530

    bash bug fix (script wasn't running)

--- generate-config.sh
+++ generate-config.sh
@@ -5,11 +5,10 @@ truncate -s 0 blank.txt
 STREAM_NUM=$1
 OUT_NUM=$2
 
-for ((i=1; i<=$STREAM_NUM; i++)); do
-    for ((j=1; j<=$OUT_NUM; j++)); do
-        output="__stream${i}__out${j}__"
-        echo $output >> blank.txt
-    done
-    echo "" >> blank.txt
+for ((i = 1; i <= STREAM_NUM; i++)); do
+	for ((j = 1; j <= OUT_NUM; j++)); do
+		output="__stream${i}__out${j}__"
+		echo $output >>blank.txt
+	done
+	echo "" >>blank.txt
 done
-
