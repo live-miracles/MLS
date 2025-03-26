8d3d7a42ec98ca6d6189ea2fa350108933a73254
Nandisha <197202821+nandisha0@users.noreply.github.com>
2025-03-26 18:42:57 +0530

    minify pregenerated html

--- build-ejs.js
+++ build-ejs.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const ejs = require('ejs');
 const os = require('os');
 const path = require('path');
+const { minify } = require('html-minifier');
 
 async function getPublicIP() {
     const response = await fetch('https://api64.ipify.org?format=json');
@@ -11,11 +12,19 @@ async function getPublicIP() {
 
 function buildEjs(name, data) {
     const template = fs.readFileSync(`views/${name}.ejs`, 'utf-8');
-    const output = ejs.render(template, data, {
+    const rawHtml = ejs.render(template, data, {
         filename: path.join(__dirname, `views/${name}.ejs`),
     });
 
-    fs.writeFileSync(`public/${name}.html`, output);
+    const minifiedHtml = minify(rawHtml, {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        minifyCSS: true,
+        minifyJS: true
+    });
+
+    fs.writeFileSync(`public/${name}.html`, minifiedHtml);
     console.log(`Static HTML generated: public/${name}.html`);
 }
 
